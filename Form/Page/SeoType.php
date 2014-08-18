d1d134247812eb18f44d6d6b85ee5caca15903a4
Rich Birch <rich.birch@intudigital.co.uk>
2014-08-18 16:40:08 +0100

    removing the ability to edit slugs

--- Form/Page/SeoType.php
+++ Form/Page/SeoType.php
@@ -51,13 +51,6 @@ class SeoType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('urlRelative', null, array('label' => 'cms_page.form.seo.label.url_relative', 'required' => false))
-            ->add('urlComplete', null, array('label' => 'cms_page.form.seo.label.url_complete', 'required' => false))
-            ->add(
-                'urlCompleteMode',
-                null,
-                array('label' => 'cms_page.form.seo.label.url_complete_mode', 'required' => false)
-            )
             ->add('title', null, array('label' => 'cms_page.form.seo.label.title'))
             ->add('metaKeywords', null, array('label' => 'cms_page.form.seo.label.meta_keywords'))
             ->add('metaDescription', 'textarea', array('label' => 'cms_page.form.seo.label.meta_description'));
